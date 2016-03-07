04f1e32cced3860781d3f7e5f0c109cd4ca5fd29
char-lie <webmaster976@gmail.com>
2016-03-07 10:54:10 +0200

    Fixed Docker upgrade script
    Not using redundant sudo;\nLogging actions.

--- docker-upgrade.sh
+++ docker-upgrade.sh
@@ -1,4 +1,4 @@
-# Upgrade Docker Engine
+echo "Upgrade Docker Engine"
 apt-get update
 cp /etc/default/docker /tmp/docker.config
 apt-get purge docker-engine -y
@@ -6,9 +6,8 @@ apt-get install docker-engine -y
 mv /tmp/docker.config /etc/default/docker
 service docker restart
 
-# Upgrade Docker Compose
-DOCKER_COMPOSE_NAME=${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m`
-curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_NAME} > docker-compose
+echo "Upgrade Docker Compose to ${DOCKER_COMPOSE_VERSION}"
+curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin/docker-compose
+mv docker-compose /usr/local/bin/docker-compose
 
