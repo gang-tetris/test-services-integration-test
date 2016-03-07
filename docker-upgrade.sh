94f803b3891ce2f754a3e52fe9b482d5dd92c106
char-lie <webmaster976@gmail.com>
2016-03-07 11:10:01 +0200

    Fixed travis-ci config and Docker upgrade script
    Not using sudo for Docker Compose download

--- docker-upgrade.sh
+++ docker-upgrade.sh
@@ -1,13 +1,13 @@
 echo "Upgrade Docker Engine"
-apt-get update
-cp /etc/default/docker /tmp/docker.config
-apt-get purge docker-engine -y
-apt-get install docker-engine -y
-mv /tmp/docker.config /etc/default/docker
-service docker restart
+sudo apt-get update
+sudo cp /etc/default/docker /tmp/docker.config
+sudo apt-get purge docker-engine -y
+sudo apt-get install docker-engine -y
+sudo mv /tmp/docker.config /etc/default/docker
+sudo service docker restart
 
 echo "Upgrade Docker Compose to ${DOCKER_COMPOSE_VERSION}"
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
-mv docker-compose /usr/local/bin/docker-compose
+sudo mv docker-compose /usr/local/bin/docker-compose
 
