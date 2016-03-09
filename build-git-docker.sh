50cdc4dac8ffd9a2a1940e58285db5471553edf6
char-lie <webmaster976@gmail.com>
2016-03-09 19:27:15 +0200

    Refactored docker builder script
    Made variables names more clear

--- build-git-docker.sh
+++ build-git-docker.sh
@@ -1,19 +1,19 @@
 if [ -z "$2" ]
 then
-    echo "Usage: $0 ORGANIZATION/REPOSITORY CONTAINER [COMMIT=HEAD]"
-    echo "  - ORGANIZATION/REPOSITORY name of organization or user and repository"
+    echo "Usage: $0 REPOSITORY_PATH CONTAINER [COMMIT=HEAD]"
+    echo "  - REPOSITORY_PATH         name of organization/user and repository"
     echo "                            split by slash"
     echo "  - CONTAINER               name of container to build"
     echo "  - COMMIT                  hash of commit to build (default is HEAD)"
 fi
 
-ORGANIZATION=$1
+REPOSITORY_PATH=$1
 CONTAINER=$2
 COMMIT="${3:-HEAD}"
 
-mkdir -p "/tmp/${ORGANIZATION}"
-git clone "https://github.com/${ORGANIZATION}.git" "/tmp/${ORGANIZATION}/"
-cd "/tmp/${ORGANIZATION}"
+mkdir -p "/tmp/${REPOSITORY_PATH}"
+git clone "https://github.com/${REPOSITORY_PATH}.git" "/tmp/${REPOSITORY_PATH}/"
+cd "/tmp/${REPOSITORY_PATH}"
 git reset --hard ${COMMIT}
 sudo docker build -t ${CONTAINER} .
 
