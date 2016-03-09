df8776e8c8b447a1716c2da35f2f18e757d283c8
char-lie <webmaster976@gmail.com>
2016-03-09 16:39:01 +0200

    Updated docker building script
    Added ability to build from specific commit

--- build-git-docker.sh
+++ build-git-docker.sh
@@ -1,12 +1,19 @@
 if [ -z "$2" ]
 then
-    echo "Usage: $0 ORGANIZATION/REPOSITORY CONTAINER"
+    echo "Usage: $0 ORGANIZATION/REPOSITORY CONTAINER [COMMIT]"
     echo "  - ORGANIZATION/REPOSITORY name of organization or user and repository"
     echo "                            split by slash"
     echo "  - CONTAINER               name of container to build"
+    echo "  - COMMIT                  hash of commit to build (default is HEAD)"
 fi
+ORGANIZATION=$1
+CONTAINER=$2
+COMMIT="${3:-HEAD}"
+echo $ORGANIZATION $CONTAINER $COMMIT
 
-mkdir -p /tmp/$1
-git clone "https://github.com/${1}.git" /tmp/$1/
-sudo docker build -t $2 /tmp/$1
+mkdir -p "/tmp/${ORGANIZATION}"
+git clone "https://github.com/${ORGANIZATION}.git" "/tmp/${ORGANIZATION}/"
+cd "/tmp/${ORGANIZATION}"
+git reset --hard ${COMMIT}
+sudo docker build -t ${CONTAINER} .
 
