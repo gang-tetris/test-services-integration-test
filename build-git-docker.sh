77cd629c2c64bc88b72914e204bf497137a444cf
char-lie <webmaster976@gmail.com>
2016-03-09 18:21:38 +0200

    Refactored docker building script
    Removed redundant echo
    
    Improved usage

--- build-git-docker.sh
+++ build-git-docker.sh
@@ -1,15 +1,15 @@
 if [ -z "$2" ]
 then
-    echo "Usage: $0 ORGANIZATION/REPOSITORY CONTAINER [COMMIT]"
+    echo "Usage: $0 ORGANIZATION/REPOSITORY CONTAINER [COMMIT=HEAD]"
     echo "  - ORGANIZATION/REPOSITORY name of organization or user and repository"
     echo "                            split by slash"
     echo "  - CONTAINER               name of container to build"
     echo "  - COMMIT                  hash of commit to build (default is HEAD)"
 fi
+
 ORGANIZATION=$1
 CONTAINER=$2
 COMMIT="${3:-HEAD}"
-echo $ORGANIZATION $CONTAINER $COMMIT
 
 mkdir -p "/tmp/${ORGANIZATION}"
 git clone "https://github.com/${ORGANIZATION}.git" "/tmp/${ORGANIZATION}/"
