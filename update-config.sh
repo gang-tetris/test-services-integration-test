10fabb00caea41641f8364080c06804b65338789
char-lie <webmaster976@gmail.com>
2016-03-09 18:56:06 +0200

    Updated config modification script
    Fetching SHA by file URL
    
    Customized commit message
    
    Refactored script: added variables with clear names
    
    JSON file contains only needed information: message, content and SHA

--- update-config.sh
+++ update-config.sh
@@ -1,18 +1,30 @@
 if [ -z "$5" ]
 then
-    echo "Usage: $0 ORGANIZATION REPOSITORY FILEPATH CONTENT SHA"
+    echo "Usage: $0 ORGANIZATION REPOSITORY FILEPATH CONTENT SHA [MESSAGE]"
     echo "  - ORGANIZATION organization or username"
     echo "  - REPOSITORY   repository name"
-    echo "  - FILEPATH     content path"
+    echo "  - FILEPATH     path of file within repository"
     echo "  - CONTENT      file content in base64 format"
-    echo "  - SHA          blob sha of file being modified"
+    echo "  - MESSAGE      commit message (default is 'Config updated')"
     exit 1
 fi
 
-CONTENT=$(cat config-update.json | sed "s/PATH/$3/g" | sed "s/CONTENT/$4/g" | sed "s/SHA/$5/g")
+ORGANIZATION="$1"
+REPOSITORY="$2"
+PATH="$3"
+CONTENT="$4"
+MESSAGE="${5:-Config updated}"
 
-curl -s -X PUT \
+URL="https://api.github.com/repos/${ORGANIZATION}/${REPOSITORY}/contents/${PATH}"
+
+SHA=$(curl -s -X GET "${URL}" \
+    -H "Authorization: token ${GIT_CONFIG_UPDATE_TOKEN}" | \
+    grep '"sha":' | \
+    sed 's/.*\([[:xdigit:]]\{40\}\).*/\1/g')
+
+BODY=$(sed "s/SHA/${SHA}/g;s/CONTENT/${CONTENT}/g;" config-update.json)
+
+curl -s -X PUT "${URL}" \
     -H "Authorization: token ${GIT_CONFIG_UPDATE_TOKEN}" \
-    -d "$CONTENT" \
-    "https://api.github.com/repos/$1/$2/contents/$3"
+    -d "${BODY}"
 
