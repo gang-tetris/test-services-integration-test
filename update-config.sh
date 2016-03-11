70b8ca9a8c0cad09351a8fdc104f4789b0a947f0
char-lie <webmaster976@gmail.com>
2016-03-11 17:17:50 +0200

    Updated script for config update
    Reduced number of input parameters
    
    Using environment variables to build commit message automatically
    
    Making config content based on project name

--- update-config.sh
+++ update-config.sh
@@ -1,21 +1,24 @@
 if [ -z "$4" ]
 then
-    echo "Usage: $0 ORGANIZATION REPOSITORY FILEPATH CONTENT [MESSAGE]"
-    echo "  - ORGANIZATION organization or username"
-    echo "  - REPOSITORY   repository name"
-    echo "  - FILEPATH     path of file within repository"
-    echo "  - CONTENT      file content in base64 format"
-    echo "  - MESSAGE      commit message (default is 'Config updated')"
+    echo "Usage: $0 REPOSITORY_PATH PROJECT"
+    echo "  - REPOSITORY_PATH         name of organization/user and repository"
+    echo "                            split by slash"
+    echo "  - PROJECT                 name of the project being configured"
     exit 1
 fi
 
-ORGANIZATION="$1"
-REPOSITORY="$2"
-FILEPATH="$3"
-CONTENT="$4"
-MESSAGE="${5:-Config updated}"
+REPOSITORY_PATH="$1"
+PROJECT="$2"
 
-URL="https://api.github.com/repos/${ORGANIZATION}/${REPOSITORY}/contents/${FILEPATH}"
+FILEPATH="${PROJECT}-config.sh"
+VARS_PREFIX=$(tr '[:lower:]-' '[:upper:]_' ${PROJECT})
+
+CONTENT=$(base64 <(echo "export ${VARS_PREFIX}=\"${TRAVIS_COMMIT}\"") -w 0)
+COMMIT_DESCRIPTION="Changes ${TRAVIS_COMMIT_RANGE} in ${PROJECT}"
+COMMIT_TITLE="Triggering tests"
+MESSAGE="${COMMIT_TITLE}\\\n\\\n${COMMIT_DESCRIPTION}"
+
+URL="https://api.github.com/repos/${REPOSITORY_PATH}/contents/${FILEPATH}"
 
 SHA=$(curl -s -X GET "${URL}" \
     -H "Authorization: token ${GIT_CONFIG_UPDATE_TOKEN}" | \
