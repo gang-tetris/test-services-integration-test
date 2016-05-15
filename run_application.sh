94d4ad54d3f1771695c6e0436a2f8117e07dc220
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-15 18:27:44 +0300

    Executing applications

---
+++ run_application.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run -d \
+    --link rabbit:rabbit --link cassandra:cassandra --link hazelcast:hazelcast \
+    gangtetris/cassandra-repository
+docker run -d \
+    --link rabbit:rabbit \
+    gangtetris/test-amqp-server
+docker run -d \
+    --link rabbit:rabbit \
+    gangtetris/test-amqp-client
+
