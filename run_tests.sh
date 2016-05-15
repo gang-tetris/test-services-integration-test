7a894ebc9db228188f1cd6968412059d99aa7e68
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-15 19:07:12 +0300

    Fixed tests
    Running HAProxy as a daemon

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 cd haproxy
 docker build -t gangtetris/haproxy:1.6 .
-docker run --rm --name haproxy --link s1:s1 -p 80:80 -e SERVERS="s1" \
+docker run -d --name haproxy --link s1:s1 -p 80:80 -e SERVERS="s1" \
        gangtetris/haproxy:1.6
 cd -
 
