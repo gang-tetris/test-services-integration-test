90686481ea5a8ed4407bef07ca35a33aaa95c23a
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-15 17:45:31 +0300

    Running gang-tetris Hazelcast

--- run_services.sh
+++ run_services.sh
@@ -1,5 +1,5 @@
 docker run -d --name cassandra -h cassandra gangtetris/cassandra:2.2
-docker run -d --name hazelcast -h hazelcast hazelcast/hazelcast:3.6
+docker run -d --name hazelcast -h hazelcast gangtetris/hazelcast
 docker run -d --name rabbit -h rabbit rabbitmq:3
 
 until docker exec cassandra cqlsh -e quit
@@ -7,7 +7,10 @@ do
     sleep 1
 done
 
-cd /tmp/gangtetris/test-cassandra-repository
+REPOSITORY_PATH="test-cassandra-repository"
+mkdir -p "/tmp/${REPOSITORY_PATH}"
+git clone "https://github.com/${REPOSITORY_PATH}.git" "/tmp/${REPOSITORY_PATH}/"
+cd "/tmp/${REPOSITORY_PATH}"
 exec run_migrations.sh
 cd -
 
@@ -16,9 +19,9 @@ do
     sleep 1
 done
 
-#HAZELCAST_URL="http://0.0.0.0:5701/hazelcast/rest/management/cluster/state"
-#until curl --data "app1&app1-pass" "${HAZELCAST_URL}"
-#do
-#    sleep 1
-#done
+HAZELCAST_URL="http://0.0.0.0:5701/hazelcast/rest/management/cluster/state"
+until curl --data "app1&app1-pass" "${HAZELCAST_URL}"
+do
+    sleep 1
+done
 
