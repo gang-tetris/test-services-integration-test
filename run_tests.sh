3c1a0a129c92b8c4a78543837701b97174abff5e
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-15 18:47:52 +0300

    Implemented basic tests

---
+++ run_tests.sh
@@ -0,0 +1,8 @@
+cd haproxy
+docker build -t gangtetris/haproxy:1.6 .
+docker run --rm --name haproxy --link s1:s1 -p 80:80 -e SERVERS="s1" \
+       gangtetris/haproxy:1.6
+cd -
+
+curl localhost:80/Anonymous
+
