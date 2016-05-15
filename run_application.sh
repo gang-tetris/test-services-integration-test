26d2e06644e99e5b642adbf96185bc086484c55b
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-15 21:18:36 +0300

    Added names for containers with applications

--- run_application.sh
+++ run_application.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker run -d \
+docker run -d --name repository \
     --link rabbit:rabbit --link cassandra:cassandra --link hazelcast:hazelcast \
     gangtetris/test-cassandra-repository
-docker run -d \
+docker run -d --name logic \
     --link rabbit:rabbit \
     gangtetris/test-amqp-server
 docker run -d --name s1 -h s1 \
